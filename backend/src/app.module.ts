cb857f18d18b7e302d98e6a9f7be5bd9ddcbef0f
mingnhi <luongbaotam2905@gmail.com>
2025-05-06 02:59:57 +0700

    update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 @Module({
+  // imports: [MongooseModule.forRoot('mongodb://localhost:27017/flutterproject')],
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/flutterproject'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL as string),
   ],
   controllers: [AppController],
   providers: [AppService],

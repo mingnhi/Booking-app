b76aa7a79e467945252309690ffc41eaf2fa22ba
mingnhi <128677507+mingnhi@users.noreply.github.com>
2025-05-05 21:18:55 +0700

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://your-frontend-domain.com', // Thay bằng domain frontend của bạn
+    methods: 'GET,POST,PUT,DELETE', // Các phương thức HTTP được phép
+    allowedHeaders: 'Content-Type, Authorization', // Các header được phép
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
